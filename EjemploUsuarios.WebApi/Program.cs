6b4a2d794e779a0ed5c618bb827237b338fba9da
Gonzalo <gonzaperez2312@gmail.com>
2023-10-01 17:24:23 -0300

    Use Cors

--- EjemploUsuarios.WebApi/Program.cs
+++ EjemploUsuarios.WebApi/Program.cs
@@ -3,13 +3,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseAuthorization();
-
 app.MapControllers();
+
+// Habilita CORS
+app.UseCors(builder =>
+{
+    builder
+        .AllowAnyOrigin() // Reemplaza con el dominio de tu sitio web
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+});
+
+app.UseAuthorization();
 
 app.Run();
